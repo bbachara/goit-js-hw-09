17b181e8591d71195fd65597401825ee4401dbb0
Bartek Bachara <bbachara@poczta.onet.pl>
2024-02-08 23:37:51 +0100

    js-hw-09 task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+let interval;
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', function () {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  interval = setInterval(function () {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', function () {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(interval);
+});
