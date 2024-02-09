1844cf5711ff8341672bf866a5b91d62e14c8c66
Bartek Bachara <bbachara@poczta.onet.pl>
2024-02-09 21:29:31 +0100

    js-hw-09 task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,69 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+    if (selectedDate <= now) {
+      window.alert('Please choose a date in the future');
+      return;
+    }
+    startBtn.disabled = false;
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  const endDate = new Date(document.getElementById('datetime-picker').value);
+  const timerInterval = setInterval(() => {
+    const currentTime = new Date();
+    const remainingTime = endDate - currentTime;
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      return;
+    }
+    const timeParts = convertMs(remainingTime);
+
+    const remainingDays = document.querySelector('[data-days]');
+    remainingDays.textContent = addLeadingZero(timeParts.days);
+
+    const remainingHours = document.querySelector('[data-hours]');
+    remainingHours.textContent = addLeadingZero(timeParts.hours);
+
+    const remainingMinutes = document.querySelector('[data-minutes]');
+    remainingMinutes.textContent = addLeadingZero(timeParts.minutes);
+
+    const remainingSeconds = document.querySelector('[data-seconds]');
+    remainingSeconds.textContent = addLeadingZero(timeParts.seconds);
+  }, 1000);
+});
